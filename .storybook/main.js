b93c3d97eb4289c61140d1c6f14328a8f0115b6f
devdanalmeida@gmail.com <devdanalmeida@gmail.com>
2023-05-19 11:21:33 -0300

    fix: fix: update sdk expo to 47 and update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,11 @@
 module.exports = {
-  stories: [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web", 
-    '@storybook/addon-knobs',
-    '@storybook/addon-a11y'
-  ],
-  framework: "@storybook/react",
+  stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-react-native-web", '@storybook/addon-knobs', '@storybook/addon-a11y', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
